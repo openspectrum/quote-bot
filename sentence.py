9a428cae29009029f64e4ce8f2a420a60c4945d2
Tanner Welsh <tannerwelsh@gmail.com>
2015-07-28 11:51:52 -0400

    Generate sentences with specified length
    ```
    $ python sentence.py 12 source/*.txt
    > random words Sorted grammar and ignoring.
    ```

--- sentence.py
+++ sentence.py
@@ -8,8 +8,24 @@ def allWords(files):
         allWords.extend(words)
     return allWords
 
+def sampleWord(source):
+    import random
+    index = random.randint(0, len(source) - 1)
+    return source[index]
+
+def buildSentence(length, sourceWords):
+    sentence = []
+    for i in range(length):
+        sentence.append(sampleWord(sourceWords))
+    return ' '.join(sentence) + '.'
+
+def sentence(length, files):
+    words = allWords(files)
+    return buildSentence(length, words)
 
 if __name__ == "__main__":
     import sys
-    files = sys.argv[1:len(sys.argv)]
-    print(allWords(files))
+    sentenceLength = sys.argv[1]
+    files = sys.argv[2:len(sys.argv)]
+    sent = sentence(int(sentenceLength), files)
+    print(sent)
