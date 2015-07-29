818bdd1333284fcc0ac13a7283bbc0052f621aaf
Tanner Welsh <tannerwelsh@gmail.com>
2015-07-29 12:06:49 -0400

    Use frequency distribution to select words w/ probability

--- sentence.py
+++ sentence.py
@@ -25,24 +25,40 @@ def wordCount(words):
             counts[word] = 1
     return counts
 
-def sampleWord(source):
+def sampleWord(sortedProbabilities):
     import random
-    index = random.randint(0, len(source) - 1)
-    return source[index]
+    limit = sortedProbabilities[-1][0]
+    choice = round(random.uniform(0, limit), 6)
+    for freq, word in sortedProbabilities:
+        if choice < freq:
+            return word
 
-def buildSentence(length, sourceWords):
+def probability(word, freqDist):
+    numWords = 0
+    for _, freq in freqDist.items():
+        numWords += freq
+    return round(freqDist[word] / numWords, 6)
+
+def probabilities(freqDist):
+    probabilities = []
+    prob = 0
+    for word, freq in freqDist.items():
+        prob = round(prob + probability(word, freqDist), 6)
+        probabilities.append((prob, word))
+    return sorted(probabilities)
+
+def sentence(length, words):
+    freqDist = wordCount(words)
+    sortedProbabilities = probabilities(freqDist)
     sentence = []
     for i in range(length):
-        sentence.append(sampleWord(sourceWords))
+        sentence.append(sampleWord(sortedProbabilities))
     return ' '.join(sentence) + '.'
-
-def sentence(length, files):
-    words = allWords(files)
-    return buildSentence(length, words)
 
 if __name__ == "__main__":
     import sys
     sentenceLength = sys.argv[1]
     files = sys.argv[2:len(sys.argv)]
-    sent = sentence(int(sentenceLength), files)
+    words = allWords(files)
+    sent = sentence(int(sentenceLength), words)
     print(sent)
