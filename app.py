8ceb4397acab845afc1583397e3069a626bd0b69
Tanner Welsh <tannerwelsh@gmail.com>
2015-08-05 17:40:12 -0400

    Use random quote length; use same source for all requests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+
 import pickle
+import random
 
 from words import words
 from quote import quote
@@ -7,12 +9,13 @@ from markov import make_prefix_walker
 
 source_file = 'twain-russell.markov_data'
 
+quote_length = random.randrange(10, 30)
+source = pickle.load(open(source_file, "rb"))
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    quote_length = 21
-    source = pickle.load(open(source_file, "rb"))
     walker = make_prefix_walker(source)
     return quote(quote_length, walker)
 
