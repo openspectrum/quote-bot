1b1563ef3ef436dfca26105042fa3a493612c6ae
Tanner Welsh <tannerwelsh@gmail.com>
2015-08-21 14:12:36 -0400

    Write Twitter module for sending tweets
    Expects keys and secrets to be stored as env variables

---
+++ twitter.py
@@ -0,0 +1,19 @@
+import os
+
+consumer_key = os.environ.get('TWITTER_CONSUMER_KEY')
+consumer_secret = os.environ.get('TWITTER_CONSUMER_SECRET')
+access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+from requests_oauthlib import OAuth1Session
+
+session = OAuth1Session(consumer_key,
+                        client_secret=consumer_secret,
+                        resource_owner_key=access_token,
+                        resource_owner_secret=access_token_secret)
+
+tweet_url = 'https://api.twitter.com/1.1/statuses/update.json'
+
+def tweet(status):
+    r = session.post(tweet_url, { 'status': status })
+    return r.text
